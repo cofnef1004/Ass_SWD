faf34f325b6a79fc444ecf390408071da9104d05
cofnef1004 <aduy024@gmail.com>
2023-07-08 23:13:57 +0700

    Add project files.

---
+++ Ass_SWD/Pages/Shared/_MainLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ass_SWD.Pages.Shared
+{
+    public class _MainLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

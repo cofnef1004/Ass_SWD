1554237ef27e412cb911d7865dabf684b24c1d9b
baspdd <duypdhe160308@fpt.edu.vn>
2023-07-10 22:01:03 +0700

    s

---
+++ Ass_SWD/Pages/Insurance/Create.cshtml.cs
@@ -0,0 +1,45 @@
+using Ass_SWD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Ass_SWD.Pages.Insurance
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Models.Insurance Insurance { get; set; } = default!;
+        private List<String> type = new List<String> { "Health", "Treatment", "Medical" };
+        public IActionResult OnGet()
+        {
+            ViewData["notice"] = "";
+            ViewData["type"] = new SelectList(type);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            Insurance.PatientId = 1;
+            using (var db = new MyStoreContext())
+            {
+                if (db.Insurances.Any(x => x.PatientId == Insurance.PatientId && x.Number == Insurance.Number))
+                {
+                    ViewData["notice"] = "Insurance already exist";
+                    ViewData["type"] = new SelectList(type);
+                    return Page();
+                }
+                ViewData["notice"] = "Insurance success";
+                ViewData["type"] = new SelectList(type);
+                db.Insurances.Add(Insurance);
+                db.SaveChanges();
+                return Page();
+            }
+            //return RedirectToPage("/Admin/Products/Index");
+        }
+    }
+}

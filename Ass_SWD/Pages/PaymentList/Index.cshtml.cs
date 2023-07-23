f8cbf784b40b067b0c09dcf97de5bb5030ff1aff
Nguyễn Văn Tuyên <vantuyenvk13@gmail.com>
2023-07-24 00:05:58 +0700

    fix_code

--- Ass_SWD/Pages/PaymentList/Index.cshtml.cs
+++ Ass_SWD/Pages/PaymentList/Index.cshtml.cs
@@ -11,8 +11,8 @@ namespace Ass_SWD.Pages.PaymentList
    
     public class IndexModel : PageModel
     {
-        public readonly Models.MyStoreContext _context = new Models.MyStoreContext();
-        public List<Models.Payment> payments = new List<Models.Payment>();
+       MyStoreContext _context = new MyStoreContext();
+        public List<Payment> payments = new List<Payment>();
         
         public void OnGet(string billingInformation)
         {
